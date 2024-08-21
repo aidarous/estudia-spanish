8e556eced0a403e9cbee9ecef013cbfde5c2972c
aidarous <aidarous12@gmail.com>
2024-08-21 16:23:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { AppBar, Typography, Stack, Button} from '@mui/material';
+
+import Vocabulary from './components/Vocabulary';
+import Idioms from './components/Idioms';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <AppBar position='static'>
+        <Typography
+            variant="h6"
+            component="div"
+            href="#app-bar-with-responsive-menu"
+            sx={{flexGrow:1}}
+          >
+            ESTUDIA SPANISH
+          </Typography>
+          <Stack direction='row' spacing={2}>
+            <Button color='inherit'>Home</Button>
+            <Button color='inherit'>Vocabulary</Button>
+            <Button color='inherit'>Idioms</Button>
+          </Stack>  
+        </AppBar> 
+
+        <Link to={'/vocabulary'}>Vocabulary</Link>
+        <Link to={'/idioms'}>Idioms</Link>
+
+        <Router>
+          <Routes>
+            <Route path="/vocabulary" element={<Vocabulary />} />
+            <Route path="/idioms" element={<Idioms />} />  
+          </Routes>
+        </Router>    
+        
+      </div>
+    
+    
   );
 }
 
---
+++ src/components/Idioms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Idioms = () => {
+  return (
+    <div>
+      <h1>Idioms</h1>
+    </div>
+  )
+}
+
+export default Idioms

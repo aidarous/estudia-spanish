f6fd27f560c1985ab3d1eeca21d4a3a260c260d5
aidarous <aidarous12@gmail.com>
2024-10-03 09:06:53 -0400

    added directions to travel page

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -11,8 +11,17 @@ const fetchFromFirestore = async () =>{
   });
   return data;
 }
+const fetchDirectionsFromFirestore = async () =>{
+  const querySnapshot = await getDocs(collection(db, "directions"))
+  const data = []
+  querySnapshot.forEach((doc)=>{
+    data.push({ id:doc.id, ...doc.data()})
+  });
+  return data;
+}
 const Travel = () => {
   const [travelData, setTravelData] = useState([]);
+  const [directionsData, setDirectionsData] = useState([]);
     
     useEffect(() => {
       const fetchData = async () => {
@@ -22,21 +31,59 @@ const Travel = () => {
       fetchData();
     },[])
 
+    useEffect(() => {
+      const fetchData = async () => {
+        const data = await fetchDirectionsFromFirestore();
+        setDirectionsData(data)
+      }
+      fetchData();
+    },[])
+
+const [showSurvivalPhrases, setShowSurvivalPhrases] = useState(true)
+const [showDirections, setShowDirections] = useState(true)
+const toggleSurvivalPhrases = () =>{
+  setShowSurvivalPhrases(!showSurvivalPhrases)
+}
+const toggleDirections = () =>{
+  setShowDirections(!showDirections)
+}
   return (
     <div>
       <h1>Travel</h1>
 
-      <h3> Survival phrases</h3>
+      <button onClick={toggleSurvivalPhrases}><h3> Survival phrases</h3></button>
       <div>
-      {travelData.map((travel)=>(
-        <Box>
-
         
-          <CardContent key={travel.id}className='mb-4'>
-            <Typography variant='h6'><strong>{travel.word}</strong> - {travel.translation}</Typography>
-          </CardContent>
-        </Box> 
-      ))}
+        {showSurvivalPhrases && (
+          <div>
+            {travelData.map((travel) => (
+              <Box key={travel.id}>
+                <CardContent className='mb-4'>
+                  <Typography variant='h6'>
+                    <strong>{travel.word}</strong> - {travel.translation}
+                  </Typography>
+                </CardContent>
+              </Box>
+            ))}
+          </div>
+        )}
+      </div>
+      <div>
+      <button onClick={toggleDirections}><h3> Directions</h3></button>
+        {showDirections && (
+          <div>
+            {directionsData.map((directions) => (
+              <Box key={directions.id}>
+                <CardContent className='mb-4'>
+                  <Typography variant='h6'>
+                    <strong>{directions.direction}</strong> - {directions.translation}
+                  </Typography>
+                </CardContent>
+              </Box>
+            ))}
+          </div>
+        )}
+
       </div>
 
     </div>

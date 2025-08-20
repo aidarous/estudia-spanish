a726b6c07c5a0319d12c52069d3cc309a747257f
aidarous <aidarous12@gmail.com>
2025-08-20 09:15:15 -0400

    added signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { signInWithGooglePopup } from '../firebase/firebase.utils'
+import SignUpForm from './SignUpForm'
 
 const Login = () => {
+  const logGoogleUser = async () =>{
+    const response = await signInWithGooglePopup();
+  }
+
   return (
     <div className='box'>
-      <h1>Sign in</h1>
+      <SignUpForm />
+      {/* <h1>Sign in</h1>
+      <button onClick={logGoogleUser}>log in </button> */}
     </div>
   )
 }
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,40 @@
+import {React, useState} from 'react'
+
+const defaultform = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword:''
+}
+
+function SignUpForm() {
+    const [formFields, setFormFields] = useState(defaultform)
+    const { displayName, email, password, confirmPassword} = formFields;
+
+    
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]: value})
+    }
+  return (
+    <div>
+      <form onSubmit={()=> {}}>
+        <label>Display Name</label>
+        <input type='text' required onChange={handleChange} name="displayName" value={displayName} />
+        
+        <label>Email</label>
+        <input type='email' required onChange={handleChange} name="email" value={email} />
+        
+        <label>Password</label>
+        <input type='password' required onChange={handleChange} name="password" value={password} />
+        
+        <label>Confirm Password</label>
+        <input type='password' required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
+        <button type='submit'>Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm
